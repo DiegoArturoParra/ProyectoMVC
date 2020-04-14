82eb15525001905857f93230fb2abd2ab8b6978a
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2020-04-13 23:08:05 -0500

    scroll en reparación
    no se ha podido insertar dentro del jlist

--- src/main/java/edu/cundi/proyectomvc/Views/ViewPersona.java
+++ src/main/java/edu/cundi/proyectomvc/Views/ViewPersona.java
@@ -32,29 +32,21 @@ public class ViewPersona extends JFrame {
     /**
      * panel para dar diseño al formulario.
      */
-    private JPanel panel;
-    /**
-     * Contenedor.
-     */
-    private Container contenedor;
-    private GridBagLayout gridBag;
+    public JPanel panel;
 
     public ViewPersona() throws HeadlessException {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 300);
-        contenedor = getContentPane();
-//--- PANEL
+        //--- PANEL
         panel = new JPanel();
-        scroll = new JScrollPane(this.panel);
-        gridBag = new GridBagLayout();
-        contenedor.setLayout(gridBag);
+        panel.setLayout(new GridBagLayout());
+        add(panel, BorderLayout.CENTER);
         btnInsertar = new JButton("Insertar");
         btnInsertar.setPreferredSize(new Dimension(130, 30));
         listaPersona = new JList<>();
         listaPersona.setPreferredSize(new Dimension(200, 200));
-
-        
-        
+        scroll = new JScrollPane(listaPersona);
+       
         /**
          * graficos por grillas row y Column
          */
@@ -64,28 +56,20 @@ public class ViewPersona extends JFrame {
         c.weighty = 1;
 
         c.gridx = 0;
-        c.gridy = 0; 
-        
-        
-        
-        gridBag.setConstraints(scroll, c);
-        contenedor.setLayout(new BorderLayout());
-        contenedor.add(scroll);
+        c.gridy = 0;
+        panel.add(labelNombre, c);
+
         c.gridx = 1;
         c.gridy = 0;
-        
-        
-        gridBag.setConstraints(labelNombre, c);
-
         panel.add(inputNombre, c);
 
         c.gridx = 2;
         c.gridy = 0;
         panel.add(btnInsertar, c);
-
+       
         c.gridx = 1;
         c.gridy = 1;
         panel.add(listaPersona, c);
-
     }
+
 }
